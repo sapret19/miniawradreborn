37215a1d600e98d1c53601ee252504b41da57dac
Akmal18aqil <akmalaqilwahyu@gmail.com>
2023-03-05 23:26:13 +0700

    terakhir wes

--- lib/home/komponen/body.dart
+++ lib/home/komponen/body.dart
@@ -37,7 +37,6 @@ class _bodyState extends State<body> {
             child: Container(
               margin: EdgeInsets.only(top: 30),
               child: Column(
-                // crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Row(
@@ -104,7 +103,7 @@ class _bodyState extends State<body> {
                   ),
                   bunder(
                     icon: "kitab.png",
-                    text: "Kitab-kitab",
+                    text: "Kitab Syair",
                   ),
                 ],
               ),
--- lib/rute.dart
+++ lib/rute.dart
@@ -6,10 +6,24 @@ import 'package:miniawradreborn/page/page.dart';
 Map<String, WidgetBuilder> get rute {
   return <String, WidgetBuilder>{
     "/home": (BuildContext context) => Home(),
-    "/satu": (BuildContext context) => grid_page(),
     "/tawassul": (BuildContext context) =>
         page(judulpage: "Tawassul", pdfpage: "tawassul.pdf"),
     "/birrul": (BuildContext context) =>
-        page(judulpage: "Birrul Walidayn", pdfpage: "lamiyah.pdf")
+        page(judulpage: "Birrul Walidayn", pdfpage: "lamiyah.pdf"),
+    "/yasin": (BuildContext context) =>
+        page(judulpage: "Yasin & Tahlil", pdfpage: "lamiyah.pdf"),
+    "/istighosah": (BuildContext context) =>
+        page(judulpage: "Istighosah", pdfpage: "tawassul.pdf"),
+    "/waqiah": (BuildContext context) =>
+        page(judulpage: "Al-Waqiah", pdfpage: "lamiyah.pdf"),
+    "/burdah": (BuildContext context) =>
+        page(judulpage: "Burdah", pdfpage: "lamiyah.pdf"),
+    "/diba": (BuildContext context) =>
+        page(judulpage: "Diba", pdfpage: "tawassul.pdf"),
+    "/sabul": (BuildContext context) =>
+        page(judulpage: "Sabul Munjiyat", pdfpage: "lamiyah.pdf"),
+    "/dalail": (BuildContext context) =>
+        page(judulpage: "Dalail", pdfpage: "lamiyah.pdf"),
+    "/kitab": (BuildContext context) => grid_page(),
   };
 }
