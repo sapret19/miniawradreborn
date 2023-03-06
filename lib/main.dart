cad29b6d1e5f7d87e723bb62d56439316be8bfd9
sapret19 <nhnf198@gmail.com>
2023-03-06 23:37:53 +0700

    sabul

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       return GetMaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(),
-        home: const Home(),
+        home: const grid_page(),
         debugShowCheckedModeBanner: false,
         initialRoute: "/",
         routes: rute,
